e8df70cb3f24cca186f46798d6eddbe899eaeb1f
Devilian1993 <maksioczko@gmail.com>
2026-06-09 20:10:05 +0200

    feat: add product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    calories_per_100g = models.PositiveIntegerField()
+    protein_per_100g = models.PositiveIntegerField()
+    fat_per_100g = models.PositiveIntegerField()
+    carbs_per_100g = models.PositiveIntegerField()
