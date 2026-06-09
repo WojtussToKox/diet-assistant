b7ff65fa98f6eaf8e21f54ed791d7d07b5d9d4ee
Devilian1993 <maksioczko@gmail.com>
2026-06-09 20:43:05 +0200

    fix: fix Product model data types

--- products/models.py
+++ products/models.py
@@ -1,10 +1,28 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from decimal import Decimal
 
 # Create your models here.
 
 class Product(models.Model):
-    name = models.CharField(max_length=255)
-    calories_per_100g = models.PositiveIntegerField()
-    protein_per_100g = models.PositiveIntegerField()
-    fat_per_100g = models.PositiveIntegerField()
-    carbs_per_100g = models.PositiveIntegerField()
+    name = models.CharField(max_length=255, unique=True)
+    calories_per_100g = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        validators=[MinValueValidator(Decimal('0.00'))]
+    )
+    protein_per_100g = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        validators=[MinValueValidator(Decimal('0.00'))]
+    )
+    fat_per_100g = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        validators=[MinValueValidator(Decimal('0.00'))]
+    )
+    carbs_per_100g = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        validators=[MinValueValidator(Decimal('0.00'))]
+    )
