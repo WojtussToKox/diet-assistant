45bdeb4278317105cf2a709d6a9187d0a06da798
Wojciech <wojtek.p8@op.pl>
2026-06-17 12:44:23 +0200

    feat: add endpoint and register logic

--- diet_assistant/urls.py
+++ diet_assistant/urls.py
@@ -20,9 +20,15 @@ from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
 )
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/', include('products.urls')),
     path('api/', include('diets.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
