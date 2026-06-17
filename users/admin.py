c00955df03e2ebadde3cade8482e2a1bcbc34642
Wojciech <wojtek.p8@op.pl>
2026-06-17 12:34:23 +0200

    feat: create User class

--- users/admin.py
+++ users/admin.py
@@ -2,5 +2,19 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Informacje o diecie i roli', {
+            'fields': ('role', 'dietitian')
+        }),
+        ('Cechy fizyczne', {
+            'fields': ('date_of_birth', 'gender', 'height_cm', 'weight_kg')
+        }),
+    )
+    
+    list_display = ('username', 'email', 'first_name', 'last_name', 'role', 'is_staff')
+    
+    list_filter = ('role', 'is_staff', 'is_superuser', 'is_active', 'groups')
+
 admin.site.register(User, UserAdmin)
--- users/models.py
+++ users/models.py
@@ -1,4 +1,65 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator
 
 class User(AbstractUser):
-    pass
+    ROLE_CHOICES = [
+        ('DIETETITAN', 'Dietetyk'),
+        ('PATIENT', 'Pacjent'),
+        ('STANDARD', 'Użytkownik standardowy'),
+    ]
+
+    GENDER_CHOICES = [
+        ('M', 'Mężczyzna'),
+        ('F', 'Kobieta'),
+    ]
+
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES,
+        default='STANDARD',
+        verbose_name='Rola'
+    )
+
+    dietetitian = models.ForeignKey(
+        'self',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='patients',
+        verbose_name="Dietetyk prowadzący",
+        help_text="Wybierz dietetyka z listy, jeśli użytkownik jest pacjentem."
+    )
+    
+    date_of_birth = models.DateField(
+        null=True, 
+        blank=True, 
+        verbose_name="Data urodzenia"
+    )
+
+    gender = models.CharField(
+        max_length=1, 
+        choices=GENDER_CHOICES, 
+        null=True, 
+        blank=True, 
+        verbose_name="Płeć"
+    )
+
+    height_cm = models.PositiveIntegerField(
+        null=True, 
+        blank=True,
+        validators=[MinValueValidator(50)],
+        verbose_name="Wzrost (cm)"
+    )
+
+    weight_kg = models.DecimalField(
+        max_digits=5, 
+        decimal_places=2, 
+        null=True, 
+        blank=True,
+        validators=[MinValueValidator(10.00)],
+        verbose_name="Waga (kg)"
+    )
+
+    def __str__(self):
+        return f"{self.username} ({self.get_role_display()})"
